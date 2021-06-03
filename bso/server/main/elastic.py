5771f1e9bdda260dc466f23af0dab132d6ca5614
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-03 18:22:17 +0200

    [fix] Improve ES insert by removing "_id"

--- bso/server/main/elastic.py
+++ bso/server/main/elastic.py
@@ -6,16 +6,16 @@ from bso.server.main.logger import get_logger
 
 logger = get_logger(__name__)
 
-es = None
+client = None
 try:
-    es = Elasticsearch(os.getenv('ES_URL'), http_auth=(os.getenv('ES_LOGIN'), os.getenv('ES_PASSWORD')))
+    client = Elasticsearch(os.getenv('ES_URL'), http_auth=(os.getenv('ES_LOGIN'), os.getenv('ES_PASSWORD')))
 except:
     logger.debug('Cannot connect to es')
 
 
 def delete_index(index: str):
     logger.debug(f'Deleting {index}')
-    response = es.indices.delete(index=index, ignore=[400, 404])
+    response = client.indices.delete(index=index, ignore=[400, 404])
     logger.debug(response)
 
 
@@ -24,7 +24,7 @@ def reset_index(index):
         delete_index(index)
     except:
         logger.debug('Index deletion failed')
-    response = es.indices.create(
+    response = client.indices.create(
         index=index,
         body={'settings': {}, 'mappings': {}},
         ignore=400  # ignore 400 already exists code
@@ -36,13 +36,6 @@ def reset_index(index):
 
 
 def load_in_es(data, index):
-    actions = [
-        {
-            '_index': index,
-            '_id': i,
-            '_source': data
-        }
-        for i, data in enumerate(data)
-    ]
-    helpers.parallel_bulk(client=es, actions=actions, chunk_size=500, request_timeout=60)
+    actions = [{'_index': index, '_source': datum} for datum in data]
+    helpers.parallel_bulk(client=client, actions=actions, chunk_size=500, request_timeout=60)
     logger.debug(f'{len(data)} elements imported into {index}')
