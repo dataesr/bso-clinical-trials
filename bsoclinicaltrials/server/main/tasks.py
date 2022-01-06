385a0a1e39b9204587a3595d8c18b2bf5a2cce5b
eric <eric.jeangirard@recherche.gouv.fr>
2022-01-06 11:51:30 +0100

    enable parsing without harvesting

--- bsoclinicaltrials/server/main/tasks.py
+++ bsoclinicaltrials/server/main/tasks.py
@@ -23,9 +23,9 @@ def create_task_transform_load(args: dict) -> dict:
     today = datetime.date.today()
     harvest_date_ct = args.get('harvest_date_ct', f'{today}')
     harvest_date_euctr = args.get('harvest_date_euctr', f'{today}')
-    if args.get('harvest', True):
-        res_ct = harvest_parse_clinical_trials(to_harvest=True, to_parse=True, harvest_date = harvest_date_ct)
-        res_euctr = harvest_parse_euctr(to_harvest=True, to_parse=True, harvest_date = harvest_date_euctr)
+    if args.get('harvest', True) or args.get('parse', True):
+        res_ct = harvest_parse_clinical_trials(to_harvest=args.get('harvest', True), to_parse=args.get('parse', True), harvest_date = harvest_date_ct)
+        res_euctr = harvest_parse_euctr(to_harvest=args.get('harvest', True), to_parse=args.get('parse', True), harvest_date = harvest_date_euctr)
     merged_ct = merge_all(harvest_date_ct, harvest_date_euctr)
     data = enrich(merged_ct)
     current_date = datetime.date.today().isoformat()
