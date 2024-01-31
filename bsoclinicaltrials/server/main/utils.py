0bbe9889c1ad47bfbc8e8482c850e87e75a93bc7
eric <eric.jeangirard@recherche.gouv.fr>
2024-01-31 22:42:30 +0100

    fix csv generation

--- bsoclinicaltrials/server/main/utils.py
+++ bsoclinicaltrials/server/main/utils.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import requests
 import time
+import pandas as pd
 
 from dateutil import parser
 from requests.adapters import HTTPAdapter
@@ -71,44 +72,67 @@ def requests_retry_session(retries=10, backoff_factor=0.6, status_forcelist=(500
     session.mount('https://', adapter)
     return session
 
+def pandas_to_csv(df):
+    data = df.to_dict(orient='records')
+    csv_data = []
+    for d in data:
+        elt = {}
+        for f in ['ISRCTN', 'NCTId', 'WHO', 'acronym' ,'all_sources',
+                      'delay_first_results_completion', 'delay_start_completion',
+                 'delay_submission_start', 'design_allocation', 'enrollment_count',
+            'enrollment_type', 'eudraCT', 'first_publication_date',
+            'first_results_or_publication_date', 'french_location_only',
+            'has_publication_oa', 'has_publications_result', 'has_results',
+            'has_results_or_publications', 'intervention_type', 'ipd_sharing', 'ipd_sharing_description',
+            'lead_sponsor', 'lead_sponsor_type', 'location_country', 'location_facility',
+            'other_ids', 'primary_purpose', 'publication_access', 'publications_result',
+            'references', 'results_first_submit_date', 'results_first_submit_qc_date',
+            'snapshot_date', 'status', 'status_simplified', 'study_completion_date',
+            'study_completion_date_type', 'study_completion_year',
+            'study_first_submit_date', 'study_first_submit_qc_date',
+            'study_start_date', 'study_start_date_type', 'study_start_year',
+            'study_type', 'submission_temporality', 'time_perspective', 'title']:
+            if isinstance(d.get(f), str) or isinstance(d.get(f), int) or isinstance(d.get(f), float):
+                elt[f] = d[f]
+            elif isinstance(d.get(f), list):
+                if f == 'other_ids':
+                    elt[f] = '|'.join([k['id'] for k in d[f]])
+                elif f == 'references':
+                    elt[f] = '|'.join([k.get('doi') for k in d[f] if k.get('doi')])
+                elif f == 'publication_access':
+                    elt[f] = '|'.join(str(d[f]))
+                else:
+                    elt[f] = '|'.join(d[f])
+            elif d.get(f) is None:
+                elt[f] = None
+            else:
+                print(f)
+                print(type(d[f]))
+                assert(False)
+        csv_data.append(elt)
+    df_csv = pd.DataFrame(csv_data)
+    return df_csv
+
 
 def dump_to_object_storage(args: dict) -> list:
+    file_suffix = args.get('file_suffix', '')
     es_index = args.get('index_name', 'bso-clinical-trials')
     # 1. Dump ES bso-clinical-trials index data into temp file
     es_url_without_http = ES_URL.replace('https://', '').replace('http://', '')
     es_host = f'https://{ES_LOGIN_BSO_BACK}:{parse.quote(ES_PASSWORD_BSO_BACK)}@{es_url_without_http}'
     container = 'bso_dump'
-    today = datetime.date.today().isoformat().replace('-', '')
     os.makedirs(MOUNTED_VOLUME, exist_ok=True)
-    output_json_file = f'{MOUNTED_VOLUME}{es_index}_{today}.jsonl.gz'
-    output_csv_file = f'{MOUNTED_VOLUME}{es_index}_{today}.csv'
+    output_json_file = f'{MOUNTED_VOLUME}{es_index}{file_suffix}.jsonl.gz'
+    output_csv_file = f'{MOUNTED_VOLUME}{es_index}{file_suffix}.csv'
     cmd_elasticdump = f'elasticdump --input={es_host}{es_index} --output={output_json_file} ' \
                       f'--type=data --sourceOnly=true --fsCompress=gzip --limit 10000'
     logger.debug(cmd_elasticdump)
     os.system(cmd_elasticdump)
     logger.debug('Elasticdump is done')
     # 2. Convert JSON file into CSV by selecting fields
-    cmd_header = f"echo 'ISRCTN,NCTId,WHO,acronym,all_sources," \
-        f"delay_first_results_completion,delay_start_completion," \
-        f"delay_submission_start,design_allocation,enrollment_count," \
-        f"enrollment_type,eudraCT,first_publication_date," \
-        f"first_results_or_publication_date,french_location_only," \
-        f"has_publication_oa,has_publications_result,has_results," \
-        f"has_results_or_publications,intervention_type,ipd_sharing,ipd_sharing_description," \
-        f"lead_sponsor,lead_sponsor_type,location_country,location_facility," \
-        f"other_ids,primary_purpose,publication_access,publications_result," \
-        f"references,results_first_submit_date,results_first_submit_qc_date," \
-        f"snapshot_date,status,status_simplified,study_completion_date," \
-        f"study_completion_date_type,study_completion_year," \
-        f"study_first_submit_date,study_first_submit_qc_date," \
-        f"study_start_date,study_start_date_type,study_start_year," \
-        f"study_type,submission_temporality,time_perspective,title'" \
-        f" > {output_csv_file}"
-    logger.debug(cmd_header)
-    os.system(cmd_header)
-    cmd_jq = f"zcat {output_json_file} | jq -rc '[.ISRCTN,.NCTId,.WHO,.acronym,((.all_sources)?|join(\";\"))//null,.delay_first_results_completion,.delay_start_completion,.delay_submission_start,.design_allocation,.enrollment_count,.enrollment_type,.eudraCT,.first_publication_date,.first_results_or_publication_date,.french_location_only,.has_publication_oa,.has_publications_result,.has_results,.has_results_or_publications,.intervention_type,.ipd_sharing,.ipd_sharing_description,.lead_sponsor,.lead_sponsor_type,((.location_country)?|join(\";\"))//null,((.location_facility)?|join(\";\"))//null,((.other_ids)?|join(\";\"))//null,.primary_purpose,((.publication_access)?|join(\";\"))//null,.publications_result,((.references)?|join(\";\"))//null,.results_first_submit_date,.results_first_submit_qc_date,.snapshot_date,.status,.status_simplified,.study_completion_date,.study_completion_date_type,.study_completion_year,.study_first_submit_date,.study_first_submit_qc_date,.study_start_date,.study_start_date_type,.study_start_year,.study_type,.submission_temporality,.time_perspective,.title]|flatten|@csv' >> {output_csv_file}"
-    logger.debug(cmd_jq)
-    os.system(cmd_jq)
+    df_json=pd.read_json(output_json_file, lines=True)
+    df_csv = pandas_to_csv(df_json)
+    df_csv.to_csv(output_csv_file, index=False, sep=',')
     cmd_gzip = f'gzip {output_csv_file}'
     logger.debug(cmd_gzip)
     os.system(cmd_gzip)
