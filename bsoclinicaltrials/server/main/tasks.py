2b00087552c038def30dea9d40c3910a6bd7d107
Anne L'Hôte <anne.lhote@gmail.com>
2024-03-22 16:38:49 +0100

    refactor(tasks): Refactor code

--- bsoclinicaltrials/server/main/tasks.py
+++ bsoclinicaltrials/server/main/tasks.py
@@ -4,8 +4,8 @@ from bsoclinicaltrials.server.main.clinical_trials import harvest_parse_clinical
 from bsoclinicaltrials.server.main.elastic import load_in_es, reset_index, update_alias
 from bsoclinicaltrials.server.main.enrich_ct import enrich
 from bsoclinicaltrials.server.main.euctr import harvest_parse_euctr
-from bsoclinicaltrials.server.main.merge_sources import merge_all
 from bsoclinicaltrials.server.main.logger import get_logger
+from bsoclinicaltrials.server.main.merge_sources import merge_all
 
 logger = get_logger(__name__)
 
@@ -25,16 +25,16 @@ def create_task_transform_load(args: dict) -> dict:
     today = datetime.date.today()
     harvest_date_ct = args.get('harvest_date_ct', f'{today}')
     harvest_date_euctr = args.get('harvest_date_euctr', f'{today}')
-    if args.get('harvest', True) or args.get('parse', True):
-        res_ct = harvest_parse_clinical_trials(to_harvest=args.get('harvest', True), to_parse=args.get('parse', True), harvest_date = harvest_date_ct)
-        res_euctr = harvest_parse_euctr(to_harvest=args.get('harvest', True), to_parse=args.get('parse', True), harvest_date = harvest_date_euctr)
+    to_harvest = args.get('harvest', True)
+    to_parse = args.get('parse', True)
+    if to_harvest or to_parse:
+        res_ct = harvest_parse_clinical_trials(to_harvest=to_harvest, to_parse=to_parse, harvest_date=harvest_date_ct)
+        res_euctr = harvest_parse_euctr(to_harvest=to_harvest, to_parse=to_parse, harvest_date=harvest_date_euctr)
     merged_ct = merge_all(harvest_date_ct, harvest_date_euctr)
     data = enrich(merged_ct)
-    current_date = datetime.date.today().isoformat()
+    current_date = today.isoformat()
     index = args.get('index', f'bso-clinical-trials-{current_date}')
     reset_index(index=index)
     load_in_es(data=data, index=index)
-    alias = 'bso-clinical-trials'
     # alias update should be done manually !
-    # update_alias(alias=alias, old_index='bso-clinical-trials-*', new_index=index)
-    return {'nb_data': len(data), 'index': index, 'alias': alias}
+    return {'nb_data': len(data), 'index': index}
