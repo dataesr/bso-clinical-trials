24365c45498fa83d5b0b4cd6f8d3dece2ec5806e
Anne L'Hôte <anne.lhote@gmail.com>
2024-12-24 15:07:43 +0100

    fix(ctis): Correct the filter on French clinical trials

--- bsoclinicaltrials/server/main/ctis.py
+++ bsoclinicaltrials/server/main/ctis.py
@@ -40,7 +40,7 @@ def harvest():
     cts_fr = []
     for ct in cts:
         # Filter on French clinical trials
-        if "fr:" in [country.lower() for country in ct.get("trialCountries")]:
+        if "france:" in [country.lower() for country in ct.get("trialCountries")]:
             r = requests.get(
                 f"https://euclinicaltrials.eu/ctis-public-api/retrieve/{ct.get('ctNumber')}", verify=False)
             cts_fr.append(r.json())
