392f291227ab6c47afcdca10b2561d50fd7db6cd
Anne L'Hôte <anne.lhote@gmail.com>
2026-01-29 10:48:19 +0100

    feat(academic): Improve clinical trial type

--- bsoclinicaltrials/server/main/enrich_ct.py
+++ bsoclinicaltrials/server/main/enrich_ct.py
@@ -7,16 +7,18 @@ from bsoclinicaltrials.server.main.utils import chunks, get_dois_info
 
 def tag_sponsor(x):
     x_normalized = normalize(x)
-    for f in ["hopit", "hosp", "universi", "chu ", "ihu ", "cmc ", "gustave roussy", "pasteur",
-              "leon berard", " national", "calmettes", "calmette", "curie", "direction centrale", "société francaise",
-              "anrs", "inserm", "unicancer", "polyclinique", "institut régional", "hotel dieu",
-              "imagine institute", "cardiometabolisme", "cardiométabolisme", "foresight", "rythme cardiaque",
-            # CLCC
-              "lutte contre le cancer", "oscar lambret", "baclesse", "aurelle",
-              "becquerel", "roussy", "godinot", "de lorraine", "strasbourg", "institut De cancerologie",
-              "marquis", "francois leclerc", "françois leclerc", "jean perrin", "bérard", "berard",
-              "bergonié", "bergonie", "claudius regaud", "oncopole", "institut du cancer", "lacassagne", "catherine"]:
-        if f in x_normalized:
+    dictionary = ["hopit", "hosp", "universi", "chu ", "ihu ", "cmc ", "gustave roussy", "pasteur",
+        "leon berard", " national", "calmettes", "calmette", "curie", "direction centrale", "société francaise",
+        "anrs", "inserm", "unicancer", "polyclinique", "institut régional", "hotel dieu",
+        "imagine institute", "cardiometabolisme", "cardiométabolisme", "foresight", "rythme cardiaque",
+        # CLCC
+        "lutte contre le cancer", "oscar lambret", "baclesse", "aurelle",
+        "becquerel", "roussy", "godinot", "de lorraine", "strasbourg", "institut De cancerologie",
+        "marquis", "francois leclerc", "françois leclerc", "jean perrin", "bérard", "berard",
+        "bergonié", "bergonie", "claudius regaud", "oncopole", "institut du cancer", "lacassagne", "catherine"]
+    dictionary_normalized = [normalize(x) for x in dictionary]
+    for word in dictionary_normalized:
+        if word in x_normalized:
             return "academique"
     return "industriel"
 
