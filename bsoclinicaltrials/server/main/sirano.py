3415e2102f09e4ceb699f7b6c08cc6a0c89e31d6
Anne L'Hôte <anne.lhote@gmail.com>
2026-01-29 12:28:00 +0100

    fix(sirano): Add documentation /2

--- bsoclinicaltrials/server/main/sirano.py
+++ bsoclinicaltrials/server/main/sirano.py
@@ -5,7 +5,7 @@ from bsoclinicaltrials.server.main.logger import get_logger
 logger = get_logger(__name__)
 
 # https://sante.gouv.fr/systeme-de-sante/innovation-et-recherche/l-innovation-et-la-recherche-clinique/appels-a-projets/programmes-recherche
-url = "https://www.data.gouv.fr/api/1/datasets/r/56589f33-b66b-4b00-ae5c-fe9dcdc9a6e3" # MAJ December 5, 2024
+url = "https://www.data.gouv.fr/api/1/datasets/r/56589f33-b66b-4b00-ae5c-fe9dcdc9a6e3" # donnees-sirano-projets-c2-2024.csv, MAJ 2024/12/05
 
 def get_sirano():
     df = pd.read_csv(url, sep=';', encoding='iso-8859-1')
